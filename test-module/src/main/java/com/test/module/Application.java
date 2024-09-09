f5cbcb3496ddc2258b3407f5ffadfbd8a97efd3e
s <mrbunker@qq.com>
2024-09-09 14:31:33 +0800

    first-auth

--- test-module/src/main/java/com/test/module/Application.java
+++ test-module/src/main/java/com/test/module/Application.java
@@ -1,5 +1,6 @@
 package com.test.module;
 
+import cn.dev33.satoken.SaManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("启动成功");
+        // System.out.println("启动成功");
+        System.out.println("启动成功，Sa-Token 配置如下：" + SaManager.getConfig());
     }
 }
